0c8f6b9b8aebbbc61dbe6049a2be5f0ad99f4b4b
Gayathri G <gayathrigcse2022@gmail.com>
2025-07-26 21:32:05 +0530

    Footer update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -24,6 +24,12 @@ const Footer = () => {
             <p className="small mb-0">
               Providing end-to-end digital solutions for modern businesses to grow and thrive.
             </p>
+              <div className="d-flex pt-4 justify-content-center justify-content-md-start gap-3">
+              <a href="#" className="text-primary"><FaGoogle /></a>
+              <a href="#" className="text-primary"><FaTwitter /></a>
+              <a href="#" className="text-primary"><FaInstagram /></a>
+              <a href="#" className="text-primary"><FaFacebookF /></a>
+            </div>
           </div>
 
           {/* 2. Quick Links */}
@@ -42,25 +48,22 @@ const Footer = () => {
           <div className="col-md-4 col-lg-3 mb-4">
             <h6 className="fw-semibold mb-3">Contact Us</h6>
             <ul className="list-unstyled small mb-3">
-              <li className="d-flex align-items-center justify-content-center justify-content-md-start mb-2">
+              <li className="d-flex align-items-center justify-content-center justify-content-md-start mb-3">
                 <FaPhoneAlt className="me-2 text-primary" />
-                <span>+1258 3258 5679</span>
+                <span>+447778103271</span>
               </li>
-              <li className="d-flex align-items-center justify-content-center justify-content-md-start mb-2">
+              <li className="d-flex align-items-center justify-content-center justify-content-md-start mb-3">
                 <FaEnvelope className="me-2 text-primary" />
-                <span>hello@workik.com</span>
+                <span>info@bharatdigital.co.uk</span>
               </li>
               <li className="d-flex align-items-center justify-content-center justify-content-md-start">
                 <FaMapMarkerAlt className="me-2 text-primary" />
-                <span>102 street, y cross 485656</span>
+                <span>
+                  Unit 1-I, Romford Road,<br />
+                  London, England, E12 6BT
+                </span>
               </li>
             </ul>
-            <div className="d-flex justify-content-center justify-content-md-start gap-3">
-              <a href="#" className="text-primary"><FaGoogle /></a>
-              <a href="#" className="text-primary"><FaTwitter /></a>
-              <a href="#" className="text-primary"><FaInstagram /></a>
-              <a href="#" className="text-primary"><FaFacebookF /></a>
-            </div>
           </div>
         </div>
 
